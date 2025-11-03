f63b0b3770c9eb7eeedbeaf9f7c5ec0039f9a587
satouchida <suchida@oberlin.edu>
2025-11-03 09:01:50 -0500

    update absolute path to match local env

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --device=/dev/dxg --security-opt seccomp=unconfined -d --rm --name ocr_cli_runner --shm-size=256m -v /mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg -i ocr-v2-cli-py310:latest
+sudo docker run --device /dev/dxg --device /dev/dri --security-opt seccomp=unconfined -d --rm --name ocr_cli_runner --shm-size=256m -v /mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg -i ocr-v2-cli-py310:latest
