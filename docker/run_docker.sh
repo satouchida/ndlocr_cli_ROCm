acafeee7fcde42c1e96f094173b74418f390e8f5
satouchida <suchida@oberlin.edu>
2025-11-29 11:17:16 -0500

    update dockerfile and container creation

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,1 +1,6 @@
-sudo docker run --device /dev/dxg --security-opt seccomp=unconfined -d --rm --name ocr_docker run --device /dev/dxg -it --rm --shm-size=8g --name ocr_cli_runner -v "/mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg":/root/input.jpeg ocr-v2-cli-py310:latest
+sudo docker run --device /dev/dxg --security-opt seccomp=unconfined -d --rm --shm-size=8g --name ocr_cli_runner \
+    -v /usr/lib/wsl/lib:/usr/lib/wsl/lib \
+    -v "/mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg":/root/input.jpeg \
+    -e LD_LIBRARY_PATH=/usr/lib/wsl/lib:/opt/conda/envs/py_3.10/lib/python3.10/site-packages/torch/lib:/opt/ompi/lib:/opt/rocm/lib:/usr/local/lib \
+    -e HSA_OVERRIDE_GFX_VERSION=10.3.0 \
+    ocr-v2-cli-py310:latest tail -f /dev/null
