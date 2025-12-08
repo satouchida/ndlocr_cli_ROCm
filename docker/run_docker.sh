2e7d97bdcea0047e831b7db49d7fa06a7a0c7be1
satouchida <suchida@oberlin.edu>
2025-12-08 16:57:24 -0500

    update run_docker

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,6 +1,6 @@
-sudo docker run --device /dev/dxg --security-opt seccomp=unconfined -d --rm --shm-size=8g --name ocr_cli_runner \
-    -v /usr/lib/wsl:/usr/lib/wsl \
-    -v "/mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg":/root/input.jpeg \
+sudo docker run --device /dev/dxg --device /dev/dri --mount type=bind,src=/usr/lib/wsl,dst=/usr/lib/wsl \
+    --security-opt seccomp=unconfined -d --rm --shm-size=8g --name ocr_cli_runner \
+    -v /mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg:/root/input.jpeg \
     -e LD_LIBRARY_PATH=/usr/lib/wsl/lib:/opt/conda/envs/py_3.12/lib/python3.12/site-packages/torch/lib:/opt/ompi/lib:/opt/rocm/lib:/usr/local/lib \
     -e HSA_OVERRIDE_GFX_VERSION=11.0.0 \
     --group-add video \
