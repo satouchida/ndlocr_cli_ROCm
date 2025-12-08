fbceb6cebdacd70548d5887a956e06909043e132
satouchida <suchida@oberlin.edu>
2025-12-07 22:02:15 -0500

    update dockerbuild.sh to make sure Docker recognizes gpu

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,6 +1,9 @@
 sudo docker run --device /dev/dxg --security-opt seccomp=unconfined -d --rm --shm-size=8g --name ocr_cli_runner \
-    -v /usr/lib/wsl/lib:/usr/lib/wsl/lib \
+    -v /usr/lib/wsl:/usr/lib/wsl \
     -v "/mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg":/root/input.jpeg \
-    -e LD_LIBRARY_PATH=/usr/lib/wsl/lib:/opt/conda/envs/py_3.10/lib/python3.10/site-packages/torch/lib:/opt/ompi/lib:/opt/rocm/lib:/usr/local/lib \
+    -e LD_LIBRARY_PATH=/usr/lib/wsl/lib:/opt/conda/envs/py_3.12/lib/python3.12/site-packages/torch/lib:/opt/ompi/lib:/opt/rocm/lib:/usr/local/lib \
     -e HSA_OVERRIDE_GFX_VERSION=11.0.0 \
+    --group-add video \
+    --group-add render \
+    --group-add $(getent group render | cut -d':' -f 3)
     ocr-v2-cli-py310:latest tail -f /dev/null
