e455caab599b69aaac4007d7ba365c2bd12114ac
satouchida <suchida@oberlin.edu>
2025-12-07 23:37:05 -0500

    update mmcv version

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -5,5 +5,5 @@ sudo docker run --device /dev/dxg --security-opt seccomp=unconfined -d --rm --sh
     -e HSA_OVERRIDE_GFX_VERSION=11.0.0 \
     --group-add video \
     --group-add render \
-    --group-add $(getent group render | cut -d':' -f 3)
-    ocr-v2-cli-py310:latest tail -f /dev/null
+    --group-add $(getent group render | cut -d':' -f 3) \
+    ocr-v2-cli-py312:latest tail -f /dev/null
