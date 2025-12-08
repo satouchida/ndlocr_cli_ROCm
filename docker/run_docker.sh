9d75d8d1c93239c6655fa9e92f4e2ac6509c7448
satouchida <suchida@oberlin.edu>
2025-12-08 18:45:06 -0500

    update run_docker.sh

--- docker/run_docker.sh
+++ docker/run_docker.sh
@@ -1,5 +1,7 @@
 sudo docker run --device /dev/dxg --device /dev/dri --mount type=bind,src=/usr/lib/wsl,dst=/usr/lib/wsl \
     --security-opt seccomp=unconfined -d --rm --shm-size=8g --name ocr_cli_runner \
+    --cap-add=SYS_PTRACE \
+    --ipc=host \
     -v /mnt/d/sator/documents/ocr-refine/宗教・カルト・法/宗教・カルト・法.jpeg:/root/input.jpeg \
     -e LD_LIBRARY_PATH=/usr/lib/wsl/lib:/opt/conda/envs/py_3.12/lib/python3.12/site-packages/torch/lib:/opt/ompi/lib:/opt/rocm/lib:/usr/local/lib \
     -e HSA_OVERRIDE_GFX_VERSION=11.0.0 \
